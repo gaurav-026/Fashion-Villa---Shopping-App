4ba19f5085b2f2e15b8aea19a27c9d5dcdc5ea72
gaurav-026 <gauravchakrawarti8@gmail.com>
2025-03-24 19:17:03 +0530

    Footer Updated

--- src/app/pages/Footer.jsx
+++ src/app/pages/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='font-semibold text-center py-4 bg-cardBackground'>
-      Design & Developed by @Gaurav Chakrawarti | 16-Aug-2024
+      Design & Developed by @Prateek Sethi | 16-Aug-2024
     </div>
   )
 }
